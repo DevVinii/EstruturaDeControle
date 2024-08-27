34452b24986d08b26eb26fd5570fe7823351893c
Gideão Vinicius <gideaoviniciusalves@gmail.com>
2024-08-26 23:16:58 -0300

    first commit

---
+++ src/Contador.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+
+		Scanner terminal = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			
+			contar(parametroUm, parametroDois);
+		
+		}catch(ParametrosInvalidosException exception){
+			
+		System.out.println("O segundo parametro não pode ser maior que o primeiro.");
+		
+	}
+}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException{
+		int contagem = parametroDois - parametroUm;
+
+		if(parametroUm > parametroDois){
+
+			throw new ParametrosInvalidosException();
+
+		}
+		else
+		{ 
+
+			for(int x = 0; x < contagem; x++){
+		System.out.println(x+1);
+
+		}
+	}
+  }
+}
